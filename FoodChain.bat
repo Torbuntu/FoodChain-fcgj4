88e6fb04b63d965f2eae4f570785893df36d361f
torbuntu <sorensentor@tuta.io>
2019-07-25 20:44:59 -0500

    Fixing windows bat

--- FoodChain.bat
+++ FoodChain.bat
@@ -1,2 +1,1 @@
-SET javacmd = %~dp0
-START %~dp0\jre\bin\javaw.exe -Xmx64M -XX:+CMSClassUnloadingEnabled -jar Leikr-0.0.9.jar 
+START %~dp0jrew\bin\javaw.exe -Xmx64M -XX:+CMSClassUnloadingEnabled -jar Leikr-0.0.9.jar 
--- Leikr.bat
+++
@@ -1,1 +0,0 @@
-START javaw -Xmx64M -XX:+CMSClassUnloadingEnabled -jar Leikr-0.0.9.jar 
